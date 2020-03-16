1980bff186b1a22673ab664abba2371bc5bd58c1
Michelle Davies <sholybonoly@gmail.com>
2020-03-16 19:50:21 +0000

    Remove the ability to pick up parital postcodes as our postcodeprocessor doesn't support this yet.

--- PostcodeFinder.py
+++ PostcodeFinder.py
@@ -19,11 +19,15 @@ class PostcodeFinder:
         # We will use these first
         # Ignore case and we don't care if there is a space between beginning and end
         # Make sure we only find whole word results by using \b delimitters
+
         matches = re.findall(r"\b" + self.prefix + '\d+ ?\d\w\w' + r"\b", text, re.IGNORECASE)
-        if len(matches) == 0:
+
+        # Our PostcodeProcessor does not support partial poscodes yet
+        # We could extend this look for partial postcodes at later point in time.
+        #if len(matches) == 0:
             # If didn't find any whole postcodes 
             # Look for partial postcode (just prefix and digit)
-            matches = re.findall(r"\b" + self.prefix + '\d+' +r"\b", text, re.IGNORECASE)
+            #matches = re.findall(r"\b" + self.prefix + '\d+' +r"\b", text, re.IGNORECASE)
 
         # get red of any spaces in postcode so they all match each other
         # make it uppercase so they are all the same
